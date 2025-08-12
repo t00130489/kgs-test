ac3cc7a73fa8f1033fa05e976dc2c4c14e6858c3
t00130489 <t00130489@gmail.com>
2025-08-12 15:02:06 +0000

    812-2m

--- version.js
+++ version.js
@@ -2,7 +2,7 @@
 // 単一箇所でアプリバージョンを管理。ブラウザ & Service Worker 共通利用。
 // バージョンを更新する際はこの値のみを書き換える。
 (function(global){
-  const APP_VERSION = '812-2t';
+  const APP_VERSION = '812-2m';
   global.APP_VERSION = APP_VERSION;
   // コンソール出力は本番では不要
 })(typeof self !== 'undefined' ? self : window);
